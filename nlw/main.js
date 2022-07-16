81dd853f777a2cc11b90b8b9157812f16ccdc7da
allyson7 <allysonlopes0@gmail.com>
2022-07-16 11:59:53 -0400

    :hammer: Editing main.js and active sections

--- nlw/main.js
+++ nlw/main.js
@@ -7,6 +7,32 @@ onScroll();
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(services);
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2;
+
+  const sectionTop = section.offsetTop;
+  const sectionHeight = section.offsetHeight;
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  console.log('topo passou da linha?', sectionTopReachOrPassedTargetLine ? 'sim' : '');
+
+  const sectionEndsAt = sectionTop + sectionHeight;
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id');
+  const menuElement = document.querySelector(`.menu a [href*=${sectionId}]`);
+
+  if (sectionBoundaries) {
+    menuElement.classList.add()
+  }
 }
 
 function showNavOnScroll() {
